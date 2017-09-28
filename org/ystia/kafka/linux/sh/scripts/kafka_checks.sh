0d12e462f2fdbdd3a2cff0ea75c1fd9f199b5271
durieuxp <philippe.durieux@atos.net>
2017-09-28 11:37:45 +0200

    Fix kafka issues

---
+++ org/ystia/kafka/linux/sh/scripts/kafka_checks.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+source ${utils_scripts}/utils.sh
+
+ensure_home_var_is_set
+
+usage () {
+    log error "${BASH_SOURCE[0]} <alien_kafka_service_name> (kafka|zookeeper) (ready|pid)"
+}
+
+check_zookeeper_ready () {
+    echo srvr | nc 127.0.0.1 2181 | egrep '(Mode: follower|Mode: leader|Mode: standalone)' > /dev/null 2>&1
+    return $?
+}
+
+check_zookeeper_pid () {
+    get_pid_from_port "2181"
+}
+
+check_kafka_pid () {
+    get_pid_from_port "9092"
+}
+
+check_kafka_ready () {
+    broker_id=${INSTANCE_ID}
+    kafka_bin_path="${KAFKA_HOME}/bin"
+    zookeeper_service_name=${KAFKA_ZK_BASE_DNS_SRV_NAME}
+    result=$(echo "get /brokers/ids/${broker_id}" | ${kafka_bin_path}/zookeeper-shell.sh ${zookeeper_service_name}:2181 2>&1)
+    if echo "$result" | grep -c "Node does not exist"; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+
+if [[ "$#" != "3" ]]; then
+    usage
+    exit 255
+fi
+
+service_name=$1
+source ${HOME}/.starlings/${service_name}-service.env
+
+component=$2
+check=$3
+
+eval "check_${component}_${check}"
+
