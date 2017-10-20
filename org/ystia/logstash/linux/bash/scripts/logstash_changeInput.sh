73c49a1cb6983dde9c50e8f35dbe7dd3b811e772
Helene Joanin <helene.joanin@atos.net>
2017-10-20 16:17:49 +0200

    BRBDCF-561: Fix Logstash custom commands

--- org/ystia/logstash/linux/bash/scripts/logstash_changeInput.sh
+++ org/ystia/logstash/linux/bash/scripts/logstash_changeInput.sh
@@ -5,6 +5,8 @@ log begin
 
 source ${scripts}/logstash_utils.sh
 
+ensure_home_var_is_set
+
 log info "Execute change input command with URL : $url"
 
 replace_conf_file $LOGSTASH_HOME/conf/1-1_logstash_inputs.conf "input" $url || error_exit "Reconfiguration failed"
--- org/ystia/logstash/linux/bash/scripts/logstash_changeOutput.sh
+++ org/ystia/logstash/linux/bash/scripts/logstash_changeOutput.sh
@@ -5,6 +5,8 @@ log begin
 
 source ${scripts}/logstash_utils.sh
 
+ensure_home_var_is_set
+
 log info "Execute change output command with URL : $url"
 
 replace_conf_file $LOGSTASH_HOME/conf/3-1_logstash_outputs.conf "output" $url || error_exit "Reconfiguration failed"
