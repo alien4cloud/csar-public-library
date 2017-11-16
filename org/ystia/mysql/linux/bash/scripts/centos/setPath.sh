338ffc93dccfa3c8f4a3d771e35b2e0d0f20de55
Helene Joanin <helene.joanin@atos.net>
2017-11-16 09:45:42 +0100

    BRBDCF-561: MySQL: Fix the relation dependsOn.MysqlVolume (sudo needed since no more root user)

--- org/ystia/mysql/linux/bash/scripts/centos/setPath.sh
+++ org/ystia/mysql/linux/bash/scripts/centos/setPath.sh
@@ -10,31 +10,31 @@ if [ -n "$device"  ]; then
     mysqlLog="$mountPoint/mysql/log"
     mysqlConf="/etc/my.cnf"
 
-    #Stop mysql
+    # Stop mysql
     log info "stopping mysql"
     sudo service mysqld stop
 
     #Disable SELinux
-    setenforce 0
+    sudo setenforce 0
 
-    #Copy mysql data
-    mkdir -p $mysqlData
-    chown mysql:mysql $mysqlData
-    mv /var/lib/mysql/* $mysqlData
+    # Copy mysql data
+    sudo mkdir -p $mysqlData
+    sudo chown mysql:mysql $mysqlData
+    sudo mv /var/lib/mysql/* $mysqlData
 
-    #Change mysql configuration
-    sed -i -e 's#datadir=.*#datadir='$mysqlData'#' $mysqlConf
-    sed -i -e 's/socket/#socket/' $mysqlConf
-    echo "[client]" >> $mysqlConf
-    echo "socket="$mysqlData"/mysql.sock" >> $mysqlConf
+    # Change mysql configuration
+    sudo sed -i -e 's#datadir=.*#datadir='$mysqlData'#' $mysqlConf
+    sudo sed -i -e 's/socket/#socket/' $mysqlConf
+    sudo sh -c "echo "[client]" >> $mysqlConf"
+    sudo sh -c "echo "socket=$mysqlData/mysql.sock" >> $mysqlConf"
 
-    #Copy mysql log
-    mkdir -p $mysqlLog
-    chown mysql:mysql $mysqlLog
-    mv /var/log/mysqld.log $mysqlLog
-    sed -i -e 's#log-error=.*#log-error='$mysqlLog'/mysqld.log#' $mysqlConf
+    # Copy mysql log
+    sudo mkdir -p $mysqlLog
+    sudo chown mysql:mysql $mysqlLog
+    sudo mv /var/log/mysqld.log $mysqlLog
+    sudo sed -i -e 's#log-error=.*#log-error='$mysqlLog'/mysqld.log#' $mysqlConf
 
-    #Start mysql
+    # Start mysql
     log info "starting mysql"
     sudo service mysqld start
 fi
