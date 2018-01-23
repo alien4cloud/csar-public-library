739d420b7ee066acac5e44764ae92e5b7bdde3d4
Helene Joanin <helene.joanin@atos.net>
2018-01-23 18:51:02 +0100

    BRBDCF-782: Undo previous changes in Python (back to V2, and to nlp_twitter, dataviz, ... properties) and in Jupyter (fixes about R and PY35 kernels installation, and no more installation of nb_conda_kernel)

--- org/ystia/jupyter/linux/bash/scripts/jupyter_create.sh
+++ org/ystia/jupyter/linux/bash/scripts/jupyter_create.sh
@@ -15,6 +15,7 @@ if isServiceInstalled; then
   exit 0
 fi
 
+ENV_JUPYTER_FILE=$HOME/.jupyter/ystia_env4jupyter.sh
 
 sudo yum install -y wget
 
@@ -26,142 +27,170 @@ echo "c.NotebookApp.port = 9888" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.token = ''" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.password = ''" >> $HOME/.jupyter/jupyter_notebook_config.py
 
+
 # Configure default directory for notebooks storage
 mkdir $HOME/MyNotebooks
 echo "c.NotebookApp.notebook_dir='"$HOME/MyNotebooks"'" >> $HOME/.jupyter/jupyter_notebook_config.py
 
 
-# install package nb_conda_kernels to add a tab in Jupyter to manage conda environment
-# https://github.com/Anaconda-Platform/nb_conda
-conda install -y nb_conda_kernels
-log info "nb_conda_kernels installed"
+if [[ "${IRKERNEL}" == "true" ]]
+then
+  #base
+  sudo yum install -y libzmq3-dev python-zmq
+  sudo yum install -y zeromq-devel
+  #gitr2 error donc pas devtools
+  sudo yum install -y openssl-devel
+  sudo yum install -y czmq-devel
+  #Unable to find the LibSSH2 library on this mais pas d'erreur
+  sudo yum install -y libssh2-devel
+  #curl
+  sudo yum install -y libcurl-devel
 
-#if [[ "${IRKERNEL}" == "true" ]]
-#then
-#  #base
-#  sudo yum install -y libzmq3-dev python-zmq
-#  sudo yum install -y zeromq-devel
-#  #gitr2 error donc pas devtools
-#  sudo yum install -y openssl-devel
-#  sudo yum install -y czmq-devel
-#  #Unable to find the LibSSH2 library on this mais pas d'erreur
-#  sudo yum install -y libssh2-devel
-#  #curl
-#  sudo yum install -y libcurl-devel
-#
-#  log info "R installation"
-#  sudo yum install -y R
-#  log info "R is installed"
-#  #install dans  nouvelle library
-#  mkdir -p $HOME/R/x86_64-redhat-linux-gnu-library/3.3
-#  sudo bash -c "echo 'export LD_LIBRARY_PATH="/lib/:/lib64/:/usr/lib64/:/usr/lib64/R/lib:$HOME/anaconda3/lib"' >> /etc/bashrc"
-#
-#  #get the export before Rscript
-#  source /etc/bashrc
-#  log info "add new library"
-#  Rscript ${data_file}/packJup.R
-#  log info "irkernel installed"
-#fi
+  log info "R installation"
+  sudo yum install -y R
+  log info "R is installed"
+  #install dans  nouvelle library
+  sudo bash -c "echo 'export LD_LIBRARY_PATH="/lib/:/lib64/:/usr/lib64/:/usr/lib64/R/lib:$HOME/anaconda2/lib"' >> /etc/bashrc"
+  #Fix issue BRBDCF-466
+  mkdir -p $HOME/R/x86_64-redhat-linux-gnu-library/3.3
+  echo "R_LIBS=$HOME/R/x86_64-redhat-linux-gnu-library/3.3" >>${ENV_JUPYTER_FILE}
 
-#if [[ "${SPARK_KERNEL}" == "true" ]]
-#then
-#
-#  #MODE OFFLINE
-#  if [[ -n "${REPOSITORY}" ]] && [[ "${REPOSITORY}" != "DEFAULT" ]] && [[ "${REPOSITORY}" != "null" ]]; then
-#    #download and install anaconda
-#
-#    cd $HOME
-#    wget "${REPOSITORY}/spark-1.5.2-bin-hadoop2.6.tgz" -P "$HOME" -N
-#    tar zxvf spark-1.5.2-bin-hadoop2.6.tgz
-#    log info "spark installed"
-#
-#    wget "${REPOSITORY}/spark-kernel-0.1.5-SNAPSHOT.tar.gz" -P "$HOME" -N
-#    tar zxvf spark-kernel-0.1.5-SNAPSHOT.tar.gz
-#    log info "spark-kernel downloaded"
-#
-#    sudo yum install -y java
-#    log info "java installed"
-#
-#    mkdir -p ~/.ipython/kernels/spark
-#    cp -r spark-kernel ~/.ipython/kernels/spark/
-#    cd ~/.ipython/kernels/spark/
-#
-#  else
-#    #MODE ONLINE
-#    cd $HOME
-#    wget "http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz" -P "$HOME" -N
-#    tar zxvf spark-1.5.2-bin-hadoop2.6.tgz
-#    log info "spark installed"
-#
-#    git clone https://github.com/ibm-et/spark-kernel.git
-#
-#    log info "spark-kernel downloaded"
-#
-#    cd spark-kernel
-#    export APACHE_SPARK_VERSION=1.5.2
-#
-#    curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
-#    sudo yum install -y sbt
-#    log info "sbt installed"
-#
-#    make dist
-#    log info "make dist"
-#
-#
-#    mkdir -p ~/.ipython/kernels/spark
-#    cp -r dist/spark-kernel ~/.ipython/kernels/spark/
-#    cd ~/.ipython/kernels/spark/
-#
-#  fi
-#  #delete the last 3 lines
-#  head -n -3 ./spark-kernel/bin/spark-kernel > tmp.txt; rm -f ./spark-kernel/bin/spark-kernel; mv tmp.txt ./spark-kernel/bin/spark-kernel
-#
-#  echo 'exec "$SPARK_HOME"/bin/spark-submit \
-#  ${SPARK_OPTS} \
-#  --driver-class-path $PROG_HOME/lib/${KERNEL_ASSEMBLY} \
-#  --class com.ibm.spark.SparkKernel $PROG_HOME/lib/${KERNEL_ASSEMBLY} "$@"
-#  ' >> ./spark-kernel/bin/spark-kernel
-#
-#  echo "{
-#    \"display_name\": \"Spark\",
-#    \"language_info\": { \"name\": \"scala\" },
-#    \"argv\": [
-#    \"$HOME/.ipython/kernels/spark/spark-kernel/bin/spark-kernel\",
-#    \"--master\",
-#    \"yarn-client\",
-#    \"--profile\",
-#    \"{connection_file}\"
-#    ],
-#    \"codemirror_mode\": \"scala\",
-#    \"env\": {
-#      \"SPARK_OPTS\": \"--driver-java-options=-Xms1024M --driver-java-options=-Xmx4096M --driver-java-options=-Dlog4j.logLevel=trace\",
-#      \"MAX_INTERPRETER_THREADS\": \"16\",
-#      \"SPARK_CONFIGURATION\": \"spark.cores.max=4\",
-#      \"CAPTURE_STANDARD_OUT\": \"true\",
-#      \"CAPTURE_STANDARD_ERR\": \"true\",
-#      \"SEND_EMPTY_OUTPUT\": \"false\",
-#      \"SPARK_HOME\": \"$HOME/spark-1.5.2-bin-hadoop2.6\",
-#      \"PYTHONPATH\": \"$HOME/spark-1.5.2-bin-hadoop2.6/python:$HOME/spark-1.5.2-bin-hadoop2.6/python/lib/py4j-0.9-src.zip\"
-#    }
-#  }" > ~/.ipython/kernels/spark/kernel.json
-#
-#  # otherwise permision denied with jupyter notebook
-#  sudo chmod 751 -R $HOME/.ipython
-#
-#fi
+  #get the export before Rscript
+  source /etc/bashrc
+  log info "add new library"
+  Rscript ${data_file}/packJup.R
 
+  log info "irkernel installed"
+fi
+
+if [[ "${PY35KERNEL}" == "true" ]]
+then
+  echo y | conda create -n py35 python=3.5 anaconda
+  log info "env for python35 created"
+  source activate py35
+  #otherwise permission denied
+  sudo bash -c 'source /etc/bashrc; ipython kernel install'
+  source deactivate
+  # To see py35 in the 'New' menu of Notebook
+  # Cf https://stackoverflow.com/questions/30492623/using-both-python-2-x-and-python-3-x-in-ipython-notebook
+  cp -r $HOME/anaconda2/envs/py35/share/jupyter/kernels/python3 $HOME/anaconda2/share/jupyter/kernels
+  log info "py35 kernel installed"
+fi
+
+
+# TODO Will be taken into account when MapR/Hortonworks will be migrated...
+if [[ "${SPARK_KERNEL}" == "true" ]]
+then
+
+  #MODE OFFLINE
+  if [[ -n "${REPOSITORY}" ]] && [[ "${REPOSITORY}" != "DEFAULT" ]] && [[ "${REPOSITORY}" != "null" ]]; then
+    #download and install anaconda
+
+    cd $HOME
+    wget "${REPOSITORY}/spark-1.5.2-bin-hadoop2.6.tgz" -P "$HOME" -N
+    tar zxvf spark-1.5.2-bin-hadoop2.6.tgz
+    log info "spark installed"
+
+    wget "${REPOSITORY}/spark-kernel-0.1.5-SNAPSHOT.tar.gz" -P "$HOME" -N
+    tar zxvf spark-kernel-0.1.5-SNAPSHOT.tar.gz
+    log info "spark-kernel downloaded"
+
+    sudo yum install -y java
+    log info "java installed"
+
+    mkdir -p ~/.ipython/kernels/spark
+    cp -r spark-kernel ~/.ipython/kernels/spark/
+    cd ~/.ipython/kernels/spark/
+
+  else
+    #MODE ONLINE
+    cd $HOME
+    wget "http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz" -P "$HOME" -N
+    tar zxvf spark-1.5.2-bin-hadoop2.6.tgz
+    log info "spark installed"
+
+    git clone https://github.com/ibm-et/spark-kernel.git
+
+    log info "spark-kernel downloaded"
+
+    cd spark-kernel
+    export APACHE_SPARK_VERSION=1.5.2
+
+    curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+    sudo yum install -y sbt
+    log info "sbt installed"
+
+    make dist
+    log info "make dist"
+
+
+    mkdir -p ~/.ipython/kernels/spark
+    cp -r dist/spark-kernel ~/.ipython/kernels/spark/
+    cd ~/.ipython/kernels/spark/
+
+  fi
+  #delete the last 3 lines
+  head -n -3 ./spark-kernel/bin/spark-kernel > tmp.txt; rm -f ./spark-kernel/bin/spark-kernel; mv tmp.txt ./spark-kernel/bin/spark-kernel
+
+  echo 'exec "$SPARK_HOME"/bin/spark-submit \
+  ${SPARK_OPTS} \
+  --driver-class-path $PROG_HOME/lib/${KERNEL_ASSEMBLY} \
+  --class com.ibm.spark.SparkKernel $PROG_HOME/lib/${KERNEL_ASSEMBLY} "$@"
+  ' >> ./spark-kernel/bin/spark-kernel
+
+  echo "{
+    \"display_name\": \"Spark\",
+    \"language_info\": { \"name\": \"scala\" },
+    \"argv\": [
+    \"$HOME/.ipython/kernels/spark/spark-kernel/bin/spark-kernel\",
+    \"--master\",
+    \"yarn-client\",
+    \"--profile\",
+    \"{connection_file}\"
+    ],
+    \"codemirror_mode\": \"scala\",
+    \"env\": {
+      \"SPARK_OPTS\": \"--driver-java-options=-Xms1024M --driver-java-options=-Xmx4096M --driver-java-options=-Dlog4j.logLevel=trace\",
+      \"MAX_INTERPRETER_THREADS\": \"16\",
+      \"SPARK_CONFIGURATION\": \"spark.cores.max=4\",
+      \"CAPTURE_STANDARD_OUT\": \"true\",
+      \"CAPTURE_STANDARD_ERR\": \"true\",
+      \"SEND_EMPTY_OUTPUT\": \"false\",
+      \"SPARK_HOME\": \"$HOME/spark-1.5.2-bin-hadoop2.6\",
+      \"PYTHONPATH\": \"$HOME/spark-1.5.2-bin-hadoop2.6/python:$HOME/spark-1.5.2-bin-hadoop2.6/python/lib/py4j-0.9-src.zip\"
+    }
+  }" > ~/.ipython/kernels/spark/kernel.json
+
+  # otherwise permision denied with jupyter notebook
+  sudo chmod 751 -R $HOME/.ipython
+
+fi
+
+
+# TODO Will be taken into account when H2O will be migrated...
 if [[ "${H2O}" == "true" ]]
 then
   conda install -y requests
+  conda install -y tabulate
+  conda install -y scikit-learn
+  conda install -y colorama
   conda install -y future
   log info "env for h2o created"
   conda install -y h2o h2o-py
   log info "h2o installed"
 fi
 
+# Take into account proxy
+[[ -r /etc/profile.d/proxy.sh ]] && (cat /etc/profile.d/proxy.sh | sed -e "s|export||" >>${ENV_JUPYTER_FILE})
 # Setup systemd service
 sudo cp ${scripts}/systemd/jupyter.service /etc/systemd/system/jupyter.service
 sudo sed -i -e "s/{{USER}}/${USER}/g" -e "s@{{CMD_JUPYTER}}@$(whereis jupyter| awk '{ print $2 }')@g" /etc/systemd/system/jupyter.service
+if [[ -r ${ENV_JUPYTER_FILE} ]]
+then
+  sudo sed -i -e "s@{{ENVIRONMENTFILE}}@EnvironmentFile=${ENV_JUPYTER_FILE}@g" /etc/systemd/system/jupyter.service
+else
+  sudo sed -i -e "s@{{ENVIRONMENTFILE}}@@g" /etc/systemd/system/jupyter.service
+fi
 sudo systemctl daemon-reload
 #sudo systemctl enable jupyter.service
 
--- org/ystia/python/linux/bash/scripts/anaconda_install.sh
+++ org/ystia/python/linux/bash/scripts/anaconda_install.sh
@@ -13,24 +13,61 @@ fi
 
 
 INSTALL_DIR=$HOME
-ANACONDA_INSTALLER_NAME=Anaconda3-5.0.1-Linux-x86_64.sh
-ANACONDA_DOWNLOAD_PATH=${REPOSITORY}/${ANACONDA_INSTALLER_NAME}
 
 
 # Install bzip2 for anaconda and wget
 sudo yum install -y bzip2 wget
 log info "bzip2 and wget installed"
 
-# Download and install anaconda
-wget "${ANACONDA_DOWNLOAD_PATH}" -P "$HOME" -N
-log info "Anaconda Downloaded"
-bash $HOME/${ANACONDA_INSTALLER_NAME} -b -p ${INSTALL_DIR}/anaconda3
-log info "Anaconda Installed"
 
-# Add to path
-sudo bash -c "echo 'export PATH="$INSTALL_DIR/anaconda3/bin:$PATH"' >> /etc/bashrc"
-log info "PATH Edited"
-source /etc/bashrc
+if [[ -n "${REPOSITORY}" ]] && [[ "${REPOSITORY}" != "DEFAULT" ]] && [[ "${REPOSITORY}" != "null" ]]; then
+    # MODE OFFLINE
+    # TODO ....
+    log info "TODO: ........"
+else
+    # MODE ONLINE
+    # Download and install anaconda
+    REPOSITORY="http://repo.continuum.io/archive"
+    ANACONDA_INSTALLER_NAME="Anaconda2-5.0.1-Linux-x86_64.sh"
+    ANACONDA_DOWNLOAD_PATH=${REPOSITORY}/${ANACONDA_INSTALLER_NAME}
+    wget "${ANACONDA_DOWNLOAD_PATH}" -P "$HOME" -N
+    log info "Anaconda Downloaded"
+    bash $HOME/${ANACONDA_INSTALLER_NAME} -b -p ${INSTALL_DIR}/anaconda2
+    log info "Anaconda Installed"
+    # Add to path
+    sudo bash -c "echo 'export PATH="$INSTALL_DIR/anaconda2/bin:$PATH"' >> /etc/bashrc"
+    log info "PATH Edited"
+    source /etc/bashrc
+
+    if [[ "${NLP_TWITTER}" == "true" ]]
+    then
+      conda install -y nltk
+      conda install -y -c conda-forge twython
+      log info "nltk and twython installed"
+    fi
+
+    if [[ "${DATAVIZ}" == "true" ]]
+    then
+      conda install -y seaborn
+      conda install -y plotly
+      sudo yum install -y python-qt4
+      log info "seaborn and plotly installed"
+    fi
+
+    if [[ "${DATAFORMAT}" == "true" ]]
+    then
+      conda install -y csvkit
+      conda install -y configparser
+      log info "csvkit and configparser installed"
+    fi
+
+    if [[ "${PYBRAIN}" == "true" ]]
+    then
+      conda install -y -c mq pybrain
+      log info "pybrain installed"
+    fi
+
+fi
 
 
 setServiceInstalled
