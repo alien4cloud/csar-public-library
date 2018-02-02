3a2cf311b25cf5270723dd45fde82f07084b1ff1
Helene Joanin <helene.joanin@atos.net>
2018-02-02 12:32:09 +0100

    BRBDCF-781: Clean up in TOSCA MySQL component and topology

--- org/ystia/mysql/linux/bash/scripts/centos/install.sh
+++ org/ystia/mysql/linux/bash/scripts/centos/install.sh
@@ -7,7 +7,7 @@ source /etc/profile
 log info "Configure YUM repositories"
 mysql_repo_file="/etc/yum.repos.d/mysql-community.repo"
 sudo bash -c "cat $scripts/centos/mysql-community.repo > $mysql_repo_file"
-sudo sed -i -e "s,MYSQL_COMMUNITY_REPO,${MYSQL_COMMUNITY_REPO},g" $mysql_repo_file
+sudo sed -i -e "s,MYSQL_REPO,${MYSQL_REPO},g" $mysql_repo_file
 
 # Install MySQL community server
 log info "-> install MySQL server"
