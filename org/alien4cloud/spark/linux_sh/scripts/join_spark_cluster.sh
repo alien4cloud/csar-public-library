89bf860c92f15be380d934a91cc523f23226da59
Xavier DEGENNE <xavier@fastconnect.fr>
2018-02-09 16:37:27 +0100

    Add spark components & topologie

---
+++ org/alien4cloud/spark/linux_sh/scripts/join_spark_cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.1-bin-hadoop2.7/sbin/start-slave.sh spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
---
+++ org/alien4cloud/spark/linux_sh/scripts/start_master.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.1-bin-hadoop2.7/sbin/start-master.sh"
