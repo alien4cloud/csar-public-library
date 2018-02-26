cb4bc14dad20d65835a6c83969bd9d5fb919dabb
Mathis GAVILLON <mathis.gavillon@atos.net>
2018-02-26 14:52:08 +0100

    Add Logstash delete operation

---
+++ org/ystia/logstash/linux/bash/scripts/logstash_delete.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ${utils_scripts}/utils.sh
+
+# Try to delete the most elements
+set +e
+
+log begin
+
+log info "Remove systemd service"
+sudo rm -f /etc/systemd/system/logstash.service
+
+log info "Remove all logstash related files"
+sudo rm -rf $HOME/logstash-${LS_VERSION}
+sudo rm -rf $HOME/logstash-${LS_VERSION}.tar.gz
+
+log info "Remove all flags to avoid error when reinstalling it on a node"
+sudo rm -rf ${HOME}/.ystia/.Logstash*
+
+log end
