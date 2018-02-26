95ab844da99cef0522a871372a1537e3ad671794
Mathis GAVILLON <mathis.gavillon@atos.net>
2018-02-26 14:52:55 +0100

    Improve Elasticsearch delete operation

--- org/ystia/elasticsearch/linux/bash/scripts/elasticsearch_delete.sh
+++ org/ystia/elasticsearch/linux/bash/scripts/elasticsearch_delete.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 source ${utils_scripts}/utils.sh
+
+set +e
+
 log begin
 
 ensure_home_var_is_set
 
 log info "Remove systemd service"
-rm -f /etc/systemd/system/elasticsearch.service
+sudo rm -f /etc/systemd/system/elasticsearch.service
 
 log info "Remove all elasticsearch related files"
-rm -rf ${HOME}/elasticsearch-${ES_VERSION}
-rm -f ${HOME}/elasticsearch-${ES_VERSION}.tar.gz
-rm -f ${HOME}/elasticsearch-${ES_VERSION}.tar.gz.sha512
+sudo rm -rf ${HOME}/elasticsearch-${ES_VERSION}
+sudo rm -f ${HOME}/elasticsearch-${ES_VERSION}.tar.gz
+sudo rm -f ${HOME}/elasticsearch-${ES_VERSION}.tar.gz.sha512
 
 log info "Remove all flags to avoid error when reinstalling it on a node"
-rm -rf ${HOME}/.ystia/.Elasticsearch-*
+sudo rm -rf ${HOME}/.ystia/.Elasticsearch-*
 
 log end
