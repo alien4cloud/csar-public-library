16d3dea8f795a617baab23eedb7dae7724450434
Mathis GAVILLON <mathis.gavillon@atos.net>
2018-02-27 13:11:34 +0100

    Improve Consul delete script

--- org/ystia/consul/linux/bash/scripts/consul_delete.sh
+++ org/ystia/consul/linux/bash/scripts/consul_delete.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 source ${utils_scripts}/utils.sh
+
+set +e
+
 log begin
 
 ensure_home_var_is_set
 
 log info "Remove systemd service"
-rm -f /etc/systemd/system/consul.service
+sudo rm -f /etc/systemd/system/consul.service
 
 log info "Remove all consul related files"
 rm -rf ${HOME}/consul
 
 log info "Remove all flags to avoid error when reinstalling it on a node"
 rm -rf ${HOME}/.ystia/consul*
+rm -rf ${HOME}/.ystia/.Consul*
 
 log end
