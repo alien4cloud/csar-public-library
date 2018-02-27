ca2274a91f8ec784d83a365a7b93d42b42690256
Mathis GAVILLON <mathis.gavillon@atos.net>
2018-02-27 13:10:54 +0100

    Revert back to sharding and replica configuration

--- org/ystia/elasticsearch/linux/bash/scripts/elasticsearch_start.sh
+++ org/ystia/elasticsearch/linux/bash/scripts/elasticsearch_start.sh
@@ -19,11 +19,12 @@ wait_for_port_to_be_open "127.0.0.1" "9300" 240  || error_exit "Cannot open port
 sleep 3
 
 # Configure shards and replicas
-TEMPLATE='http://localhost:9200/_template/template_all'
-CURL='/usr/bin/curl'
-OP='-XPUT -H "Content-Type: application/json"'
-PAYLOAD="{\"template\" : \"*\", \"order\" : 0, \"settings\" : {\"number_of_shards\" : ${number_of_shards}, \"number_of_replicas\" : ${number_of_replicas} }}"
-log info "Command: $CURL $OP $TEMPLATE -d '${PAYLOAD}'"
-$CURL $OP $TEMPLATE -d '${PAYLOAD}' || error_exit "Failed executing Elasticsearch configuration with number_of_shards ${number_of_shards} and number_of_replicas ${number_of_replicas}" $?
+# TODO : fix issue setting sharding and replica configuration
+#TEMPLATE='http://localhost:9200/_template/template_all'
+#CURL='/usr/bin/curl'
+#OP='-XPUT'
+#PAYLOAD="{\"template\" : \"*\", \"order\" : 0, \"settings\" : {\"number_of_shards\" : ${number_of_shards}, \"number_of_replicas\" : ${number_of_replicas} }}"
+#log info "Command: $CURL $OP $TEMPLATE -d '${PAYLOAD}'"
+#$CURL $OP $TEMPLATE -d '${PAYLOAD}' || error_exit "Failed executing Elasticsearch configuration with number_of_shards ${number_of_shards} and number_of_replicas ${number_of_replicas}" $?
 
 log end
