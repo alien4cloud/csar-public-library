30923eee255551674140e60c171e39bf3cb220d4
Helene Joanin <helene.joanin@atos.net>
2018-03-06 16:27:41 +0100

    BRBDCF-1089: Import a dashboard script uses now Kibana API export/import

--- org/ystia/kibana/linux/bash/scripts/ystia_import_dashboard.sh
+++ org/ystia/kibana/linux/bash/scripts/ystia_import_dashboard.sh
@@ -13,19 +13,6 @@ log begin
 ensure_home_var_is_set
 source ${YSTIA_DIR}/kibana_env.sh
 
-ELASTICSEARCH_URL="http://localhost:9200"
-KIBANA_INDEX=".kibana"
-
-
-# Workaround to solve the mapping problem (long instead int) for the fields 'hits' and 'version' to fix the discovery problem
-curl -XPUT "${ELASTICSEARCH_URL}/${KIBANA_INDEX}"
-curl -XPUT "${ELASTICSEARCH_URL}/${KIBANA_INDEX}/_mapping/search" -d'{"search": {"properties": {"hits": {"type": "integer"}, "version": {"type": "integer"}}}}'
-curl -XPUT "${ELASTICSEARCH_URL}/${KIBANA_INDEX}/_mapping/visualization" -d'{"visualization": {"properties": {"hits": {"type": "integer"}, "version": {"type": "integer"}}}}'
-curl -XPUT "${ELASTICSEARCH_URL}/${KIBANA_INDEX}/_mapping/dashboard" -d'{"dashboard": {"properties": {"hits": {"type": "integer"}, "version": {"type": "integer"}}}}'
-
-
-CMD="python2 ${scripts}/ystia_import_dashboard.py -f ${DASHBOARD_FILE} -es ${ELASTICSEARCH_URL} -k ${KIBANA_INDEX}"
-log info "$CMD"
-$CMD
+curl -XPOST http://localhost:5601/api/kibana/dashboards/import -H 'Content-type:application/json' -H 'kbn-xsrf:true' -d@${DASHBOARD_FILE}
 
 log end
