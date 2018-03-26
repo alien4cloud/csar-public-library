eba1f54db2ad695bdc28381ad9af2ddc4f36c025
Helene Joanin <helene.joanin@atos.net>
2018-03-26 10:42:12 +0200

    BRBDCF-1091: Improve Consul delete operation

--- org/ystia/consul/linux/bash/scripts/consul_delete.sh
+++ org/ystia/consul/linux/bash/scripts/consul_delete.sh
@@ -8,14 +8,19 @@ log begin
 
 ensure_home_var_is_set
 
+# TODO : DnsMasq uninstall ??
+
 log info "Remove systemd service"
+#sudo systemctl disable
 sudo rm -f /etc/systemd/system/consul.service
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
 
 log info "Remove all consul related files"
-rm -rf ${HOME}/consul
+rm -rf ${INSTALL_DIR}
 
 log info "Remove all flags to avoid error when reinstalling it on a node"
-rm -rf ${HOME}/.ystia/consul*
-rm -rf ${HOME}/.ystia/.Consul*
+rm -rf ${YSTIA_DIR}/consul_env.sh
+rm -rf ${YSTIA_DIR}/.${NODE}*
 
 log end
