10d07226311571e2f8043084611be756061b2820
Helene Joanin <helene.joanin@atos.net>
2018-03-28 13:59:11 +0200

    BRBDCF-1091: Improve delete operation of ELK components

--- org/ystia/logstash/linux/bash/scripts/logstash_delete.sh
+++ org/ystia/logstash/linux/bash/scripts/logstash_delete.sh
@@ -9,12 +9,15 @@ log begin
 
 log info "Remove systemd service"
 sudo rm -f /etc/systemd/system/logstash.service
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
 
 log info "Remove all logstash related files"
 sudo rm -rf $HOME/logstash-${LS_VERSION}
-sudo rm -rf $HOME/logstash-${LS_VERSION}.tar.gz
+sudo rm -rf $HOME/logstash-${LS_VERSION}.tar.gz $HOME/logstash-${LS_VERSION}.tar.gz.sha512 $HOME/logstash-${LS_VERSION}.tar.gz.sha1
+sudo rm -rf ${YSTIA_DIR}/${NODE}-service.env
 
 log info "Remove all flags to avoid error when reinstalling it on a node"
-sudo rm -rf ${HOME}/.ystia/*Logstash*
+sudo rm -rf ${YSTIA_DIR}/.${NODE}*
 
 log end
