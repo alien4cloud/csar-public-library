447ca37d613aca55b8bc83bb4cb91f82674ce91b
Albertin Loic <loic.albertin@atos.net>
2018-12-10 11:21:55 +0100

    Use ip addresses to advertise hostname within a spark cluster

--- org/alien4cloud/spark/linux_sh/scripts/join_spark_cluster.sh
+++ org/alien4cloud/spark/linux_sh/scripts/join_spark_cluster.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
-sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/sbin/start-slave.sh spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
+echo "Starting Spark Slave: advertising address ${SPARK_SLAVE_ADDRESS}, master: spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
+sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/sbin/start-slave.sh -h ${SPARK_SLAVE_ADDRESS} spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
--- org/alien4cloud/spark/linux_sh/scripts/start_master.sh
+++ org/alien4cloud/spark/linux_sh/scripts/start_master.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
-sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/sbin/start-master.sh"
+echo "Starting Spark Master: advertising address ${SPARK_SLAVE_ADDRESS}"
+sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/sbin/start-master.sh -h ${SPARK_MASTER_ADDRESS}"
