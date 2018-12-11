dde1974f237f37d91f8e16ff08cbc61c4e3b992e
Albertin Loic <loic.albertin@atos.net>
2018-12-11 16:54:14 +0100

    Refactor some spark capabilities Also allow to use an alternative download url for spark

--- org/alien4cloud/spark/linux_sh/scripts/start_master.sh
+++ org/alien4cloud/spark/linux_sh/scripts/start_master.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
-echo "Starting Spark Master: advertising address ${SPARK_SLAVE_ADDRESS}"
-sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/sbin/start-master.sh -h ${SPARK_MASTER_ADDRESS}"
+echo "Starting Spark Master: advertising address ${SPARK_MASTER_ADDRESS}"
+
+echo "spark.master.rest.port ${SPARK_MASTER_REST_PORT}" | sudo tee ${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/conf/spark-defaults.conf > /dev/null
+
+sudo bash -c "${SPARK_INSTALL_DIR}/spark-2.2.2-bin-hadoop2.7/sbin/start-master.sh -h ${SPARK_MASTER_ADDRESS} --webui-port ${SPARK_UI_PORT}  -p ${SPARK_MASTER_PORT}"
