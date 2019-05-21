825e0ff770ede9859b8cba4af44caccd25e80a06
Laurent Ganne <ganne.laurent@gmail.com>
2019-05-21 16:29:52 +0000

    Added steps to stop and cleanup A4C

---
+++ org/alien4cloud/alien4cloud/webapp/scripts/alien/cleanup_alien.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+source $commons/commons.sh
+
+require_envs "DATA_DIR SERVER_PROTOCOL"
+
+if [ "$SERVER_PROTOCOL" == "https" ]; then
+
+  AC4_SSL_DIR=/etc/alien4cloud/ssl
+  echo "Cleaning up $AC4_SSL_DIR directory"
+
+  sudo /bin/rm -Rf $AC4_SSL_DIR
+
+fi
+
+echo "Cleaning up $DATA_DIR directory"
+sudo /bin/rm -Rf $DATA_DIR
+
