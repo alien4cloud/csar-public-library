8c4162fc909722f85e2fc7b0959cee604b0d43e3
Laurent Ganne <ganne.laurent@gmail.com>
2019-05-29 08:50:53 +0000

    Added start interfaces to workaround UI status

---
+++ org/alien4cloud/java/jdk/linux/scripts/start_noop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# Nothing to do here, this is a container
+# Just using a start interface implementation here to have Alien4Cloud UI
+# show the deployment is done.
+
+echo "Done"
+
--- org/alien4cloud/spark/linux_sh/scripts/join_spark_cluster.sh
+++ org/alien4cloud/spark/linux_sh/scripts/join_spark_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-echo "Starting Spark Slave: advertising address ${SPARK_SLAVE_ADDRESS}, master: spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
-sudo bash -c "${SPARK_INSTALL_DIR}/spark-*/sbin/start-slave.sh -h ${SPARK_SLAVE_ADDRESS} spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
+echo "Starting Spark Worker: advertising address ${SPARK_WORKER_ADDRESS}, master: spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
+sudo bash -c "${SPARK_INSTALL_DIR}/spark-*/sbin/start-slave.sh -h ${SPARK_WORKER_ADDRESS} spark://${SPARK_MASTER_ADDRESS}:${SPARK_MASTER_PORT}"
