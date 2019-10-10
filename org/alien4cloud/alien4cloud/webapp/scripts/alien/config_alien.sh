84cd37ebe511e81a8e2959d0f81cac734b16757d
Sébastien LAURENT <sebastien.laurent@atos.net>
2019-10-10 13:26:19 +0200

    Fix for correct yaml generation

--- org/alien4cloud/alien4cloud/webapp/scripts/alien/config_alien.sh
+++ org/alien4cloud/alien4cloud/webapp/scripts/alien/config_alien.sh
@@ -57,8 +57,8 @@ else
   sudo sed -i -e "s/client\: \(.*\)/client\: true/g" ${A4C_CONFIG}
   sudo sed -i -e "s/# transportClient\: \(.*\)/transportClient\: true/g" ${A4C_CONFIG}
   # set the elsaticsearch hosts
-  sudo sed -i -e "s/  hosts\: \(.*\)/hosts\: $es_list/g" ${A4C_CONFIG}
-  sudo sed -i -e "s/# hosts\: \(.*\)/hosts\: $es_list/g" ${A4C_CONFIG}
+  sudo sed -i -e "s/  hosts\: \(.*\)/  hosts\: $es_list/g" ${A4C_CONFIG}
+  sudo sed -i -e "s/# hosts\: \(.*\)/  hosts\: $es_list/g" ${A4C_CONFIG}
 
   # get the cluster name
   cluster_name=$(</tmp/a4c/work/${NODE}/cluster_name)
